a18070c5691a4a92b23dc3689fd32bfd9bf27c5d
Pablo Pizarro <pablo@ppizarror.com>
2015-10-01 22:33:02 -0300

    Updated release.py

--- lib/release.py
+++ lib/release.py
@@ -11,7 +11,7 @@
 # Constantes
 AUTOR_NAME = "Pablo Pizarro"
 AUTOR_NAME_EMAIL = "pablo@ppizarror.com"
-PROGRAM_VERSION = "0.5.3"
+PROGRAM_VERSION = "0.5.4"
 PROGRAM_TITLE = "Hero of Antair"
 
 # Propiedades del proyecto
